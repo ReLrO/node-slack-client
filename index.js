5274e2eeba09bf5599c482d389537b427c0e516e
Ariel Rosenthal <relrosen@hotmail.com>
2016-01-26 15:35:49 +0200

    added legacy RTM client

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   WebClient: require('./lib/clients/web/client'),
   RtmClient: require('./lib/clients/rtm/client'),
+  LegacyRTMClient: require('./lib/clients/default/legacy-rtm'),
   EVENTS: require('./lib/clients/events'),
 };
